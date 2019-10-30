e6b50f845792f2db136174d04f2060910fe72053
hts <tksxogks@gmail.com>
2019-10-30 15:47:31 +0900

    add TextView

--- app/src/main/java/kr/co/htssoft/githubtest/MainActivity.java
+++ app/src/main/java/kr/co/htssoft/githubtest/MainActivity.java
@@ -3,12 +3,17 @@ package kr.co.htssoft.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       tv = findViewById(R.id.tv);
     }
 }
